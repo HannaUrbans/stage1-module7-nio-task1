ef6cdb9531c33c8b3de4d356a9614c5081c56d37
Hanna_Urban <huan92@yandex.ru>
2024-02-14 00:21:51 +0300

    done

--- src/main/java/com/epam/mjc/nio/FileReader.java
+++ src/main/java/com/epam/mjc/nio/FileReader.java
@@ -1,11 +1,111 @@
 package com.epam.mjc.nio;
 
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.nio.ByteBuffer;
+import java.nio.channels.FileChannel;
+import java.nio.charset.StandardCharsets;
+import java.util.HashMap;
+import java.util.Map;
 
 
 public class FileReader {
+    private String name;
+    private Integer age;
+    private String email;
+    private Long phone;
 
-    public Profile getDataFromFile(File file) {
-        return new Profile();
+
+    public String getName() {
+        return name;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public Long getPhone() {
+        return phone;
+    }
+
+    public static Profile getDataFromFile(File file) {
+        String fileData = readFileData(file);
+        try {
+            Map<String, String> keyValuePairs = parseKeyValuePairs(fileData);
+            {
+                for (Map.Entry<String, String> entry : keyValuePairs.entrySet()) {
+                    //String key = entry.getKey();
+                    //String value = entry.getValue();
+                }
+            }
+            String name = keyValuePairs.get("Name");
+            int age = Integer.parseInt(keyValuePairs.get("Age"));
+            String email = keyValuePairs.get("Email");
+            long phone = Long.parseLong(keyValuePairs.get("Phone"));
+
+            return new Profile(name, age, email, phone);
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+    private static String readFileData(File file) {
+        StringBuilder sb = new StringBuilder();
+        try (FileInputStream fis = new FileInputStream(file)) {
+            FileChannel channel = fis.getChannel();
+            ByteBuffer buffer = ByteBuffer.allocate(1024);
+            while (channel.read(buffer) != -1) {
+                buffer.flip();
+                sb.append(StandardCharsets.UTF_8.decode(buffer).toString());
+                buffer.clear();
+            }
+        } catch (Exception e) {
+            System.out.println("An error occurred: " + e.getMessage());
+        }
+        return sb.toString();
+    }
+
+    public static Map<String, String> parseKeyValuePairs(String fileData) {
+        Map<String, String> keyValuePairs = new HashMap<>();
+        String[] parsedLines = fileData.split("\n");
+        for (String parsedLine : parsedLines) {
+            String[] words = parsedLine.split(":");
+            if (words.length == 2) {
+                String key = words[0].trim();
+                String value = words[1].trim();
+                keyValuePairs.put(key, value);
+            }
+        }
+        return keyValuePairs;
+    }
+
+
+    public void main(String[] args) throws IOException {
+
+        try {
+            File file = new File("src/main/resources/Profile.txt");
+            Profile profile = getDataFromFile(file);
+            System.out.println("Name: " + this.getName());
+            System.out.println("Age: " + this.getAge());
+            System.out.println("Email: " + this.getEmail());
+            System.out.println("Phone: " + this.getPhone());
+
+
+        } catch (Exception e) {
+            System.out.println("An error occurred: " + e.getMessage());
+            System.out.println("Stack trace:");
+            for (StackTraceElement element : e.getStackTrace()) {
+                System.out.println(element.toString());
+            }
+        }
+
     }
 }
+
+
